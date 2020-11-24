e3ee00edf5a899df5c983cebddc87e99d18300d3
giuntac <cecilia.giunta@student.h-farm.com>
2020-11-24 15:36:46 +0100

    created a class for all the snake functionalities

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-'''This is out main working file'''
-
-# Start from creating the snake
+'''This is our main working file'''
 
 from turtle import Screen, Turtle
 import time 
@@ -10,69 +8,23 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 snake = Snake() #calling class
-# the class creates the snake body
+# the class creates the snake body, moves it and allows for control with keys
 
-#screen.update()
-#2. move the snake automatically
-
+screen.listen()       
+screen.onkey(snake.up,'Up')
+screen.onkey(snake.down,'Down')
+screen.onkey(snake.left,'Left')
+screen.onkey(snake.right,'Right')
+      
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1) 
-    
-    for seg_num in range(len(segments)-1,0,-1):
-    #start where we start the range, stop and step - we want to go from 2 1, step -1
-        new_x= segments[seg_num -1].xcor()
-        new_y=segments[seg_num -1].ycor()
-        segments[seg_num].goto(new_x, new_y)#we want the third segment to go to the second position
-#we have to move also the first segment away 
-    segments[0].forward(20)
-    segments[0].left(90)
-    #the pieces will do only a circle 
-    
-    #we need to think about segments that move in a graph
-
-#3 Control the snake using the up-down-left-right arrow keys.
-
-screen.listen()       
-screen.onkey('Up')
-screen.onkey('Down')
-screen.onkey('Left')
-screen.onkey('Right')
-
-
-head = segments[0]
-up = 90
-down = 270
-left = 180
-right = 0 
-
-#Move the head of the snake (segment[0]) by 0,90,180 or 270 degrees.
-
-def up():
-    if head.heading() != down: 
-        head.setheading(up) #if it goes up, it cannot go down
-def down():
-    if head.heading() != up: 
-        head.setheading(down) #if it goes down, it cannot go up 
-def left():
-    if head.heading() != right: 
-        head.setheading(left) #if it goes left, it cannot go right
-def right():
-    if head.heading() != left: 
-        head.setheading(right) #if it goes right, it cannot go left
-      
-                                  
+    time.sleep(0.1)                                  
                                       
                                       
                                       
-                                      
-
-
-
-
-
-
+                                
 screen.exitonclick() #run it it does not disappear straight away 
--- snake.py
+++ snake.py
@@ -2,7 +2,12 @@
 
 from turtle import Screen, Turtle
 
-STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]  
+STARTING_POSITIONS = [(0,0),(-20,0),(-40,0)]
+MOVE_DISTANCE = 20  
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
 
@@ -19,3 +24,25 @@ class Snake:
 	        new_segment.goto(position)
 	        self.segments.append(new_segment)
 
+	def move(self):
+	    for seg_num in range(len(self.segments)-1,0,-1):
+                new_x= self.segments[seg_num -1].xcor()
+                new_y=self.segments[seg_num -1].ycor()
+                self.segments[seg_num].goto(new_x, new_y)
+	    self.head.forward(MOVE_DISTANCE)
+
+	def up(self):
+	    if self.head.heading()!= DOWN:
+	        self.head.setheading(UP)
+
+	def down(self):
+	    if self.head.heading()!= UP:
+	        self.head.setheading(DOWN)
+
+	def left(self):
+	    if self.head.heading()!= RIGHT:
+	        self.head.setheading(LEFT)
+
+	def right (self):
+	    if self.head.heading()!= LEFT:
+	        self.head.setheading(RIGHT)
