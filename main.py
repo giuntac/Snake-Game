4c84b05356f8e2abc84d6dd42fc534ebb730ae05
povelatorachele <povelatorachele@gmail.com>
2020-11-25 12:24:45 +0100

    Modified main

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)                                  
-                                      
+                                    
                                       
                                       
                                 
