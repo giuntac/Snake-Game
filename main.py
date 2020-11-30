2d96b5f17e75f6429b78f936e5fcb272fa89d80c
FrancescaMichielan <72807274+FrancescaMichielan@users.noreply.github.com>
2020-11-30 12:04:45 +0100

    solved issue with food collision, add features

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 '''This is our main working file'''
 
-from turtle import Screen, Turtle
+from turtle import Screen  #Turtle we can delate Turtle since we use it inside all the classes
 import time 
 from snake import Snake 
+from food import Food 
+from scoreboard import Scoreboard
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,6 +15,8 @@ screen.tracer(0)
 
 snake = Snake() #calling class
 # the class creates the snake body, moves it and allows for control with keys
+food = Food()
+scoreboard = Scoreboard() #allow to maintain the score and create the scoreboard
 
 screen.listen()       
 screen.onkey(snake.up,"Up")
@@ -23,8 +28,13 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)      
+    snake.move()  
 
-    snake.move()                            
+    # detect collision with food
+
+    If snake.head.distance(food) < 15:
+	food.refresh()
+	scoreboard.increase_score()                          
                                       
                                       
                                                                     
