762177e5aaeb7f60dfb4d152d35ebe2a60286179
povelatorachele <povelatorachele@gmail.com>
2020-12-03 13:48:11 +0100

    Respect PEP-8 rules in food.py

--- food.py
+++ food.py
@@ -1,13 +1,16 @@
+'''This class includes the code for creating the food'''
+
 from turtle import Turtle
 import random
 
+
 class Food(Turtle):
-    
+
     def __init__(self):
         super().__init__()
         self.shape('circle')
         self.penup()
-        self.shapesize(stretch_len=0.5 , stretch_wid=0.5) 
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color('blue')
         self.speed('fastest')
         self.refresh()
