93846f1683852d9328e6bc3b9e4a8b4e46c171cd
FrancescaMichielan <72807274+FrancescaMichielan@users.noreply.github.com>
2020-12-11 10:43:45 +0100

    created the package

---
+++ package_snake/scoreboard.py
@@ -0,0 +1,57 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    """The Scoreboard class helps keep track of the user's score.
+
+    Turtle subclass.    
+
+    The score is written at the top of the screen. When the snake collides with a piece of food
+    the score is updated. At the end, the user's score is fetched to be stored in the database.
+    
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()  # Remove white arrows
+        self.goto(0, 270)
+        self.hideturtle()  # Remove the arrow
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        """Updates the score on the screen.
+        
+        When the snake collides with a piece of food the score is 
+        increased with increase_score() and the latest number is written at the top of the screen.
+        """
+        self.write(f"Score: {self.score}", align="center",
+                   font=("Courier New", 24, "normal"))
+
+    def game_over(self):
+        """Writes game over on the screen.
+
+        The game ends when the snake collides with either 
+        the walls or its own tail. When this happens, this function 
+        writes 'GAME OVER' at the centre of the screen.
+        """
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center",
+                   font=("Courier New", 24, "normal"))
+
+    def increase_score(self):
+        """Increases score when necessary.
+        
+        When the snake collides with a piece of food
+        the score is increased by one point.
+        """
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+    
+    def get_score(self):
+        """Fetches the user's final score.
+
+        return : the score at the end of the game (type: int)
+        """
+        return self.score
---
+++ package_snake/snake.py
@@ -0,0 +1,106 @@
+import csv
+from turtle import Screen, Turtle
+from configurator import get_attributes
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+attributes = get_attributes('config.csv')
+
+
+class Snake:
+    """Controls everything snake-related from appearance to behavior.
+
+    The functions included are used to create, move
+    and control (with arrowkeys) the snake.
+    Functions:
+    - create_snake : creates snake body
+    - add_segment : adds segment to snake body
+    - extend : extends snake body
+    - move : moves snake body
+    - up : makes snake go up
+    - down : makes snake go down
+    - left : makes snake go left
+    - right : makes snake go right
+
+    """
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        """Creates the snake body on the screen.
+
+        Three white squares are drawn on the screen at the chosen
+        starting poistions.
+        """
+        for position in STARTING_POSITIONS:
+            # Add a segment to the position we are looping through
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        """Adds a segment to the snake when it eats food.
+        ----------
+        Parameters
+        ----------
+        position : coordinated at which to add a segment
+        """
+        new_segment = Turtle("square")
+        new_segment.color(attributes[1])
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        """Extends the snake body with a new segment.
+
+        The new segment is added to the position of the last segment
+        """
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        """Makes the snake body move continuously.
+        """
+        for seg_num in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        """Control the snake with arrowkeys.
+
+        If the snake is going up it can't go down.
+        """
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        """Control the snake with arrowkeys.
+
+        If the snake is going down it can't go up.
+        """
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        """Control the snake with arrowkeys.
+
+        If the snake is going left it can't go right.
+        """
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        """Control the snake with arrowkeys.
+
+        If the snake is going right it can't go left.
+        """
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
